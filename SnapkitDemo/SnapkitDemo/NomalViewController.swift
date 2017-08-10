a89fc27267ca7ceb7a7a4d98eef6b3fa22153595
FlyElephant <1061731917@qq.com>
2017-08-10 22:45:37 +0800

    ContainerView的高度不要设置死

--- SnapkitDemo/SnapkitDemo/NomalViewController.swift
+++ SnapkitDemo/SnapkitDemo/NomalViewController.swift
@@ -86,6 +86,40 @@ class NomalViewController: UIViewController {
             make.height.equalTo(200)
             make.left.right.equalTo(0)
         }
+        
+        let viewContainer = UIView(frame:CGRect.zero)
+        scrollView.addSubview(viewContainer)
+        
+        viewContainer.backgroundColor = UIColor.green
+        viewContainer.snp.makeConstraints { (make) in
+            make.edges.equalTo(UIEdgeInsets.zero)
+            make.width.equalTo(scrollView)
+        }
+        
+        let label = UILabel(frame: CGRect.zero)
+        label.font = UIFont.systemFont(ofSize: 15)
+        // 设置为0， 才可以自动换行
+        label.numberOfLines = 0
+        label.text = "一般会把用来#include的文件的扩展名叫 .h，称其为头文件。 #include文件的目的就是把多个编译单元（也就是c或者cpp文件）公用的内容，单独放在一个文件里减少整体代码尺寸"
+        viewContainer.addSubview(label)
+        // 一定要在加到父view后才可以用
+        label.snp.makeConstraints { (make) in
+            make.top.equalTo(0)
+            make.left.equalTo(10)
+            make.right.equalTo(-10)
+        }
+        
+        let view2 = UIView()
+        view2.backgroundColor = UIColor.orange
+        viewContainer.addSubview(view2)
+        view2.snp.makeConstraints { (make) in
+            make.top.equalTo(label.snp.bottom)
+            make.left.equalTo(10)
+            make.right.equalTo(-10)
+            make.height.equalTo(50)
+             make.bottom.equalTo(0)
+        }
+        
     }
     
 }
